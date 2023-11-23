726ddbe8f6c724da1da9e6e2e1156c16383ca904
reprise77 <semenzemskov03@yandex.ru>
2023-11-23 19:54:48 +1000

    1. adding night mode 2. bug fixes 3. adding a parallax effect

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
-  <div class="main" @keydown="handleKeyDown" tabindex="0">
-    <h1 class="score">Очки: {{ score }}</h1>
-    <div class="game-area">
-      <div class="character"
-           :style="{ bottom: `${dinoBottom}px`, left: `${dinoLeft}px`, backgroundImage: `url(${characterFrames[frameIndex]})` }"></div>
-      <div v-for="(obstacle, index) in obstacles" :key="index" class="obstacle"
-           :style="{ bottom: `${obstacle.bottom}px`, left: `${obstacle.left}px`, backgroundImage: 'url(src/assets/spike_1.png)' }"></div>
-      <div class="ground"></div>
+    <div class="main" :class="{ 'night-mode': isNightMode }" @keydown="handleKeyDown" tabindex="0"
+         :style="{ backgroundPosition: `${parallaxOffsetLayer5}px 0, ${parallaxOffsetLayer3}px 0, ${parallaxOffsetLayer4}px 0, ${parallaxOffsetLayer2}px 0, 0 0` }">
+      <h1 class="score">Очки: {{ score }}</h1>
+      <div class="game-area">
+        <div class="character"
+             :style="{ bottom: `${dinoBottom}px`, left: `${dinoLeft}px`, backgroundImage: `url(${characterFrames[frameIndex]})` }"></div>
+        <div v-for="(obstacle, index) in obstacles" :key="index" class="obstacle"
+             :style="{ bottom: `${obstacle.bottom}px`, left: `${obstacle.left}px`, backgroundImage: 'url(src/assets/spike_1.png)' }"></div>
+        <div class="ground"></div>
+      </div>
     </div>
-  </div>
 </template>
 
 <script lang="ts">
@@ -32,6 +33,15 @@ export default {
         'src/assets/run_8.png',
       ],
       frameIndex: 0 as number,
+      parallaxOffsetLayer5: 0 as number,
+      parallaxOffsetLayer4: 0 as number,
+      parallaxOffsetLayer3: 0 as number,
+      parallaxOffsetLayer2: 0 as number,
+      parallaxLayer2Speed: 0.05 as number,
+      parallaxLayer3Speed: 1 as number,
+      parallaxLayer4Speed: 0.35 as number,
+      parallaxLayer5Speed: 0.5 as number,
+      isNightMode: false as boolean,
     };
   },
   methods: {
@@ -76,6 +86,7 @@ export default {
         if (this.obstacles[i].left < -7) {
           this.obstacles.splice(i, 1);
           this.score++;
+          this.checkNightMode();
         }
       }
     },
@@ -83,8 +94,8 @@ export default {
       const dino = {
         bottom: this.dinoBottom,
         left: this.dinoLeft,
-        right: this.dinoLeft + 30,
-        top: this.dinoBottom + 30,
+        right: this.dinoLeft + 50,
+        top: this.dinoBottom + 50,
       };
 
       for (const obstacle of this.obstacles) {
@@ -107,10 +118,42 @@ export default {
     resetGame() {
       this.obstacles = [];
       this.score = 0;
+      this.isNightMode = false;
+      this.$el.classList.remove('night-mode');
     },
     gameLoop() {
       this.moveObstacles();
       this.checkCollisions();
+    },
+    checkNightMode() {
+      if (this.score >= 10 && !this.isNightMode) {
+        this.isNightMode = true;
+        this.$el.classList.add('night-mode');
+      }
+    },
+    parallaxAnimation() {
+      setInterval(() => {
+        this.parallaxOffsetLayer3 -= this.parallaxLayer3Speed;
+        this.parallaxOffsetLayer4 += this.parallaxLayer4Speed;
+        this.parallaxOffsetLayer2 += this.parallaxLayer2Speed;
+        this.parallaxOffsetLayer5 -= this.parallaxLayer5Speed;
+        const containerWidth = 1280;
+        if (this.parallaxOffsetLayer3 <= -containerWidth) {
+          this.parallaxOffsetLayer3 = 0;
+        }
+
+        if (this.parallaxOffsetLayer4 <= -containerWidth) {
+          this.parallaxOffsetLayer4 = 0;
+        }
+
+        if (this.parallaxOffsetLayer4 <= -containerWidth) {
+          this.parallaxOffsetLayer4 = 0;
+        }
+
+        if (this.parallaxOffsetLayer5 <= -containerWidth) {
+          this.parallaxOffsetLayer5 = 0;
+        }
+      }, 20);
     },
   },
   mounted() {
@@ -122,11 +165,20 @@ export default {
         left: 1270,
       });
       this.coef += 0.05
-      setTimeout(addObstacle, (500 + Math.random() * 2000));
+      this.parallaxLayer3Speed += 0.01
+      this.parallaxLayer2Speed += 0.01
+      this.parallaxLayer4Speed += 0.01
+      this.parallaxLayer5Speed += 0.01
+      if (this.score < 50) {
+        setTimeout(addObstacle, (500 + Math.random() * 2000) + 0.05);
+      } else if (this.score >= 50) {
+        setTimeout(addObstacle, (450 + Math.random() * 1800) + 0.05);
+      }
     };
     addObstacle();
     setInterval(this.gameLoop, 15)
     this.animateCharacter();
+    this.parallaxAnimation();
   },
 };
 </script>
@@ -144,9 +196,14 @@ export default {
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  background-image: url(assets/mount1.png);
-  background-repeat: no-repeat;
+  background-image: url(assets/Layer_5.png), url(assets/Layer_3.png), url(assets/Layer_4.png), url(assets/Layer_2.png), url(assets/Layer_1.png);
+  background-repeat: repeat;
   background-size: cover;
+  transition: background-image 4s ease;
+}
+
+.main.night-mode {
+  background-image: url(assets/Layer_5_night.png), url(assets/Layer_3_night.png), url(assets/Layer_4_night.png), url(assets/Layer_2_night.png), url(assets/Layer_1_night.png);
 }
 
 .game-area {
@@ -180,8 +237,8 @@ export default {
 
 .character {
   position: absolute;
-  width: 120px;
-  height: 120px;
+  width: 140px;
+  height: 140px;
   background-size: cover;
 }
 
